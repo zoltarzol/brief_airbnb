7095ff3fbbef9d83e667f3a410963f0b2416ef9e
zoltarzol <duroisin@gmail.com>
2022-11-24 11:48:05 +0100

    creation homepage et configuration de base

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+# from .functions import squared
 
 # Create your views here.
+
+def home_view(request):
+    context = {
+        ###################
+        # TO BE COMPLETED #
+        ###################
+    }
+    # import pudb; pu.db()
+    return render(request,'home/home.html', context=context)
+
+# def about_view(request):
+#     return render(request,'divers/about_page.html')
+
+# def team_view(request):
+#     return render(request,'divers/team_page.html')
