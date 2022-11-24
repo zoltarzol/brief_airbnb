89781f15160725f0cd24770fa477bf885ccb5da1
zoltarzol <duroisin@gmail.com>
2022-11-24 10:25:34 +0100

    initialisation projet

---
+++ brief_airbnb/brief_airbnb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for brief_airbnb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brief_airbnb.settings')
+
+application = get_asgi_application()
