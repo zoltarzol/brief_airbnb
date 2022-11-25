9c2772414f70cc8d541d24a7ae2818942e887786
zoltarzol <duroisin@gmail.com>
2022-11-25 12:29:04 +0100

    struct site finie, reste CountryDetails & AboutUs

--- country_details/urls.py
+++ country_details/urls.py
@@ -3,6 +3,4 @@ from .views import country_details_view
 
 urlpatterns = [
     path('', country_details_view, name='country_details'),
-    # path('about/', about_view, name='about'),
-    # path('team/', team_view, name='team'),
 ]
